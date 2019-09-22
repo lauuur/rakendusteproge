c3da3daf1dea89d8ead5a5e51e74b2ab09e87a4d
Laur Tõnisson <43137916+lauuur@users.noreply.github.com>
2019-09-23 01:13:48 +0300

    react

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const app = express()
 const path = require("path")
-const PORT = process.env.PORT || 3000;
+const PORT = 3000
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "dist", "index.html")))
 
 app.use(express.static("dist"))
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
     console.log("Server started", PORT);
-    console.log(`http://localhost:${PORT}`);
   });
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,19 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 
 module.exports = {
   mode: "production",
-  entry: './src/index.jsx',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   plugins: [
-      new CleanWebpackPlugin(),
       new CopyPlugin([
         {
           from: "public"
         }
       ])
-    ],
-  module: {
-    rules: [
-      { test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: 'babel-loader' }
     ]
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000
-  }
 };
