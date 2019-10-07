d7add2b12273ed2db56cd8c60de3bbfafad7eae4
Laur Tõnisson <43137916+lauuur@users.noreply.github.com>
2019-10-07 15:22:45 +0300

    rest itempage

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -18,7 +18,7 @@ class HomePage extends React.PureComponent{
     }
 
     fetchItems = () => {
-        fetch("http://localhost:9000/api/items")
+        fetch("/api/items")
         .then(res =>{
             console.log("res", res);
             return res.json();
